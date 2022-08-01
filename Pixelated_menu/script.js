b110410cef00892a0658dc9ae1b6ef79e07fb5e6
HarshGa387R <hgaur701@gmail.com>
2022-08-01 15:59:56 +0530

    first commit in this repo

---
+++ Pixelated_menu/script.js
@@ -0,0 +1,9 @@
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0px";
+}
+
+function openSidebar(){
+    document.getElementById("sidebar").style.width = "170px";
+
+}
+
